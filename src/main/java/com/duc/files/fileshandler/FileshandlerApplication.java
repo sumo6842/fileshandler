54b53db107f983056f4567c275bbd1a806cd4912
sumo6842 <ductran180699@gmail.com>
2021-07-11 16:55:48 +0700

    Initial commit

---
+++ src/main/java/com/duc/files/fileshandler/FileshandlerApplication.java
@@ -0,0 +1,13 @@
+package com.duc.files.fileshandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileshandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileshandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duc/files/fileshandler/FileshandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duc.files.fileshandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileshandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
