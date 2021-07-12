399b8b0539133dd0c97f305103d1b9c1cd80f40e
sumo6842 <ductran180699@gmail.com>
2021-07-12 10:03:31 +0700

    providing uri and downloadable file on index page,

--- src/main/java/com/duc/files/fileshandler/controller/FilesController.java
+++ src/main/java/com/duc/files/fileshandler/controller/FilesController.java
@@ -2,17 +2,20 @@ package com.duc.files.fileshandler.controller;
 
 import com.duc.files.fileshandler.service.FileServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @Controller
 public class FilesController {
@@ -24,27 +27,25 @@ public class FilesController {
         this.services = services;
     }
 
-    @GetMapping("upload")
-    public String showUriFiles() {
+    @GetMapping("/upload")
+    public String showUriFile(Model model) throws IOException {
+        model.addAttribute("files",
+                services.loadAll().map(path -> MvcUriComponentsBuilder.fromMethodName(
+                            FilesController.class, "loadFile", path.getFileName().toString()
+                        ).build().toUri().toString()
+                ).collect(Collectors.toList()));
         return "index";
     }
 
-    @GetMapping("/upload/{filename.+}")
-    public ResponseEntity<File> downloadFile(@PathVariable File filename) {
-        return ResponseEntity.ok()
-                .header("Content-Disposition", "attachment;filename=" + filename)
-                .body(filename);
+    @GetMapping("download/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> loadFile(@PathVariable String filename) throws IOException {
+        var resource = services.loadFileAsResource(filename);
+        return ResponseEntity.ok().header("Content-Disposition",
+                "attachment;filename=" + filename).body(resource);
     }
 
-    /**
-     * RequestParam Vs @PathVariable: http://www.localhost/admin/duc?age=15: requestParam: age = 15 | PathVariable {"admin"} Duc
-     * PathParam vs Param
-     *
-     * @param file
-     * @param attributes
-     * @return
-     * @throws IOException
-     */
+
     @PostMapping("/files/upload")
     public String uploadFiles(@RequestParam MultipartFile file,
                               RedirectAttributes attributes) throws Exception {
--- src/main/java/com/duc/files/fileshandler/service/FileServices.java
+++ src/main/java/com/duc/files/fileshandler/service/FileServices.java
@@ -3,15 +3,21 @@ package com.duc.files.fileshandler.service;
 import com.duc.files.fileshandler.config.FileMvcConfig;
 import org.jooq.lambda.Unchecked;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class FileServices {
@@ -40,7 +46,6 @@ public class FileServices {
             throw new Exception("File is empty");
         }
         String name = file.getOriginalFilename();
-
         Path path = root.resolve(name).normalize().toAbsolutePath();
         if (!path.getParent().equals(root.toAbsolutePath())) {
             throw new Exception("Not allowed located file this folder!!!");
@@ -52,5 +57,23 @@ public class FileServices {
             System.out.println("Error: " + ex.getMessage());
         }
 
+    }
+
+    public Stream<Path> loadAll() throws IOException {
+        return Files.walk(this.root, 1)
+                .filter(path -> !path.equals(this.root));
+    }
+
+    public Resource loadFileAsResource(String filename) throws IOException {
+        Path load = load(filename);
+        Resource urlResource = new UrlResource(load.toUri());
+        if (urlResource.exists() || urlResource.isReadable()) {
+            return urlResource;
+        } else throw new IOException("Resource cant read or existed");
+
+    }
+
+    public Path load(String filename) {
+        return this.root.resolve(filename);
     }
 }
